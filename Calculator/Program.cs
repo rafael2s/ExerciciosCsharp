3b0eaeed52654ce97a497acf3a8a5efd9de88b05
Rafael Souza <rafael_popo@hotmail.com>
2023-05-05 20:36:29 -0300

    Somando de testando formas de retorno na tela

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,8 +9,16 @@ namespace Calculator
             Console.Clear();
             Console.WriteLine("Primeiro Valor: ");
             float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+            Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            Console.WriteLine("O resultado da soma é: " + (v1 + v2));
         }
     }
 }
