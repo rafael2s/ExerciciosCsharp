57ffd76aa91765a599d572b19f52e2b265cf4fea
Rafael Souza <rafael_popo@hotmail.com>
2023-05-08 22:32:40 -0300

    Começando criar a tela de menu

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -7,6 +7,9 @@ namespace EditorHTML{
 
 
             DrawScreen();
+            WriteOptions();
+
+            var opt = short.Parse(Console.ReadLine());
         }
 
         public static void DrawScreen(){
@@ -34,6 +37,23 @@ namespace EditorHTML{
 
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions(){
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("===========");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Seleciona uma opção");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir Arquivo");                                    
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair do programa!");
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opção: ");                        
         }
     }
 }
