8945e82d58a0e85d472d248f5e21b6e00de37495
Rafael Souza <rafael_popo@hotmail.com>
2023-07-24 15:07:27 -0300

    Conhecendo lib Microsoft.Data.SqlClient

--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace DataAcess;
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,1433;Database=dataacessdb;User ID=SA;Password=#sa123456;TrustServerCertificate=true"; // TrustServerCertificate=true utilizado pois estava dando erro de certificado ao tentar ler dados do banco
+
+        using (var connection = new SqlConnection(connectionString)){
+            Console.WriteLine("Conectado no SqlServerImage");
+            connection.Open();
+            
+            using(var command = new SqlCommand()){
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                var reader = command.ExecuteReader();
+                while(reader.Read()){
+                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                }
+            }
+        }
+         
+        Console.WriteLine("Hello, World!");
+    }
+}
