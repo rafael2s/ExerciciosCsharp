3c47a6d7f9e770a64c71b435d218761363dca4f7
Rafael Souza <rafael_popo@hotmail.com>
2023-07-24 15:38:54 -0300

    Instalando e conhencendo Dapper

---
+++ DataAcess/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace DataAcess.Models{
+
+    public class Category{
+        public Guid Id { get; set; }
+        public String Title { get; set; }   
+    }
+}
--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using DataAcess.Models;
 
 
 namespace DataAcess;
@@ -9,21 +11,11 @@ class Program
         const string connectionString = "Server=localhost,1433;Database=dataacessdb;User ID=SA;Password=#sa123456;TrustServerCertificate=true"; // TrustServerCertificate=true utilizado pois estava dando erro de certificado ao tentar ler dados do banco
 
         using (var connection = new SqlConnection(connectionString)){
-            Console.WriteLine("Conectado no SqlServerImage");
-            connection.Open();
-            
-            using(var command = new SqlCommand()){
-                command.Connection = connection;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
 
-                var reader = command.ExecuteReader();
-                while(reader.Read()){
-                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                }
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach(var category in categories ){
+                Console.WriteLine($"{category.Id} - {category.Title}");
             }
-        }
-         
-        Console.WriteLine("Hello, World!");
+        } 
     }
 }
