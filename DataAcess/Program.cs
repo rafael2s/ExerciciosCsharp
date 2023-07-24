c1a730726d91c53d8f4442f6c1132baca3dcb410
Rafael Souza <rafael_popo@hotmail.com>
2023-07-24 16:36:26 -0300

    Iniciando INSERT e entendendo Sql Injection

--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -10,11 +10,30 @@ class Program
     {
         const string connectionString = "Server=localhost,1433;Database=dataacessdb;User ID=SA;Password=#sa123456;TrustServerCertificate=true"; // TrustServerCertificate=true utilizado pois estava dando erro de certificado ao tentar ler dados do banco
 
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinado a serviçoes do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"INSERT INTO [Categoty] 
+                        VALUES(
+                            NEWID(), 
+                            title, 
+                            url, 
+                            summary, 
+                            order, 
+                            description, 
+                            featured)";
+
         using (var connection = new SqlConnection(connectionString)){
 
             var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach(var category in categories ){
-                Console.WriteLine($"{category.Id} - {category.Title}");
+            foreach(var item in categories ){
+                Console.WriteLine($"{item.Id} - {item.Title}");
             }
         } 
     }
