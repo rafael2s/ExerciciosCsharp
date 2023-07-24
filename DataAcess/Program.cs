5a2d4e864a0ddb9d66b0ed92799c9bf2c6174ab5
Rafael Souza <rafael_popo@hotmail.com>
2023-07-24 16:56:59 -0300

    SqlParametros e Update

--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -10,6 +10,22 @@ class Program
     {
         const string connectionString = "Server=localhost,1433;Database=dataacessdb;User ID=SA;Password=#sa123456;TrustServerCertificate=true"; // TrustServerCertificate=true utilizado pois estava dando erro de certificado ao tentar ler dados do banco
 
+        using (var connection = new SqlConnection(connectionString)){
+            UpadeteCategory(connection);
+            ListCategories(connection);
+            //CreateCategory(connection);
+        } 
+    }
+
+    static void ListCategories(SqlConnection connection){
+
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        foreach(var item in categories ){
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
+    static void CreateCategory(SqlConnection connection){
         var category = new Category();
         category.Id = Guid.NewGuid();
         category.Title = "Amazon AWS";
@@ -19,22 +35,35 @@ class Program
         category.Summary = "AWS Cloud";
         category.Featured = false;
 
-        var insertSql = @"INSERT INTO [Categoty] 
+        var insertSql = @"INSERT INTO [Category] 
                         VALUES(
-                            NEWID(), 
-                            title, 
-                            url, 
-                            summary, 
-                            order, 
-                            description, 
-                            featured)";
+                            @Id, 
+                            @Title, 
+                            @Url, 
+                            @Summary, 
+                            @Order, 
+                            @Description, 
+                            @Featured)";   
 
-        using (var connection = new SqlConnection(connectionString)){
+        var rows = connection.Execute(insertSql, new {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+        Console.WriteLine($"{rows} linhas inseridas!");
+    }
 
-            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach(var item in categories ){
-                Console.WriteLine($"{item.Id} - {item.Title}");
-            }
-        } 
+    static void UpadeteCategory(SqlConnection connection){
+        var updadeQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+        var rows = connection.Execute(updadeQuery, new{
+            id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+            title = "Frontend 2023"
+        });
+
+        Console.WriteLine($"{rows} registros atualizadas!");
     }
 }
