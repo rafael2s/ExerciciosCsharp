b22476e3b58931c24faa33a2b1d6661395788e73
Rafael Souza <rafael_popo@hotmail.com>
2023-07-25 09:22:00 -0300

    Incluindo metodos GetCategory, DeleteCategory e CreateManyCategories

--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -11,9 +11,13 @@ class Program
         const string connectionString = "Server=localhost,1433;Database=dataacessdb;User ID=SA;Password=#sa123456;TrustServerCertificate=true"; // TrustServerCertificate=true utilizado pois estava dando erro de certificado ao tentar ler dados do banco
 
         using (var connection = new SqlConnection(connectionString)){
-            UpadeteCategory(connection);
-            ListCategories(connection);
             //CreateCategory(connection);
+            //CreateManyCategories(connection);
+            //UpadeteCategory(connection);
+            //DeleteCategory(connection);
+            ListCategories(connection);
+            //GetCategory(connection);
+            
         } 
     }
 
@@ -23,6 +27,19 @@ class Program
         foreach(var item in categories ){
             Console.WriteLine($"{item.Id} - {item.Title}");
         }
+    }
+
+    static void GetCategory(SqlConnection connection)
+    {
+        var category = connection
+            .QueryFirstOrDefault<Category>(
+                "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+                new
+                {
+                    id = "af3407aa-11ae-4621-a2ef-2028b85507c4"
+                });
+        Console.WriteLine($"{category.Id} - {category.Title}");
+
     }
 
     static void CreateCategory(SqlConnection connection){
@@ -65,5 +82,68 @@ class Program
         });
 
         Console.WriteLine($"{rows} registros atualizadas!");
+    }
+    static void DeleteCategory(SqlConnection connection)
+    {
+        var deleteQuery = "DELETE [Category] WHERE [Id]=@id";
+        var rows = connection.Execute(deleteQuery, new
+        {
+            id = new Guid("eb54b251-8c1c-48e7-9214-487243cf6608"),
+        });
+
+        Console.WriteLine($"{rows} registros excluídos");
+    }
+
+    static void CreateManyCategories(SqlConnection connection){
+       
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada a serviçoes do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+         var category2 = new Category();
+        category2.Id = Guid.NewGuid();
+        category2.Title = "Google Cloud";
+        category2.Url = "google-cloud";
+        category2.Description = "Categoria destinada a serviços do Google Cloud";
+        category2.Order = 9;
+        category2.Summary = "Google Cloud";
+        category2.Featured = true;
+
+        var insertSql = @"INSERT INTO [Category] 
+                        VALUES(
+                            @Id, 
+                            @Title, 
+                            @Url, 
+                            @Summary, 
+                            @Order, 
+                            @Description, 
+                            @Featured)";   
+
+        var rows = connection.Execute(insertSql, new[] {
+            new{
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            },
+            new{
+                category2.Id,
+                category2.Title,
+                category2.Url,
+                category2.Summary,
+                category2.Order,
+                category2.Description,
+                category2.Featured
+            }
+        });
+        Console.WriteLine($"{rows} linhas inseridas!");
     }
 }
