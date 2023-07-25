1734f20e06bfd200c812f91c3070bf6d874aeffc
Rafael Souza <rafael_popo@hotmail.com>
2023-07-25 11:53:52 -0300

    Entendendo difereça entre 'Execute' e 'ExecuteScalar'

--- DataAcess/Program.cs
+++ DataAcess/Program.cs
@@ -18,7 +18,8 @@ class Program
             //ListCategories(connection);
             //GetCategory(connection);
             //ExecuteProcedure(connection);
-            ExecuteReadProcedure(connection);
+            //ExecuteReadProcedure(connection);
+            ExecuteScalar(connection);
             
         } 
     }
@@ -165,5 +166,39 @@ class Program
             //Console.WriteLine(item.Id);
             Console.WriteLine($"{item.Id} - {item.Title}");
         }
+    }
+
+    static void ExecuteScalar(SqlConnection connection){
+        var category = new Category();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinado a serviçoes do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"
+                        INSERT INTO 
+                            [Category] 
+                        OUTPUT inserted.[Id]
+                        VALUES(
+                            NEWID(), 
+                            @Title, 
+                            @Url, 
+                            @Summary, 
+                            @Order, 
+                            @Description, 
+                            @Featured) 
+                        SELECT SCOPE_IDENTITY()";   
+
+        var id = connection.ExecuteScalar<Guid>(insertSql, new {
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+        Console.WriteLine($"A categoria inserida foi: {id}");
     }
 }
